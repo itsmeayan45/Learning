86b61e2e1300de3ae9baf1fc9edb04e7bf4a4a90
Ayan Guchhait <ayanwork45@gmail.com>
2024-08-23 20:46:35 +0530

    init main

--- t2h.py
+++ t2h.py
@@ -1,7 +1,7 @@
 import pywhatkit as pw
-txt="""My name Is jaharlal Guchhait
-s/d fathers name Milan Chandra Guchhait
-live at Rasulpur, District -Hooghly
+txt="""My name Is Xamp
+s/d fathers name samc
+live at dfgh, District -fghj
 I am a small farmer and political leader.
 I want justice of RG kar Medical College incident."""
 pw.text_to_handwriting(txt,"demo1.jpeg",[0,0,138])
